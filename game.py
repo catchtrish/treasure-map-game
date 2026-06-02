86744cc0812d6615ccd7ee64b9bae6b081b2674a
catchtrish <116573848+catchtrish@users.noreply.github.com>
2026-06-01 21:03:53 -0400

    Replace js.prompt with input for user choices

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-import js
-
 print(r'''
 *******************************************************************************
           |                   |                  |                    |
@@ -26,31 +24,22 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.\n")
 
-# Prompt 1
-choice1 = str(js.prompt('You are at a crossroad. Do you want to go "left" or "right"?')).lower()
+# Back to normal Python inputs!
+choice1 = input('You are at a crossroad. Do you want to go "left" or "right"? ').lower()
 
 if choice1 == "left":
-    print(f"You chose: {choice1}\n")
-    
-    # Prompt 2
-    choice2 = str(js.prompt('Now there is a river to cross. Do you want to try swimming across, or wait for a bus? Type "swim" or "wait".')).lower()
+    choice2 = input('Now there is a river to cross. Do you want to try swimming across, or wait for a bus? Type "swim" or "wait". ').lower()
     
     if choice2 == "wait":
-        print(f"You chose: {choice2}\n")
+        choice3 = input('You arrived at the destination safe and sound. Now there is a house that has a choice of 3 doors to enter. You can choose the "yellow" door, the "blue" door, or the "red" door. Type one of the colors. ').lower()
         
-        # Prompt 3
-        choice3 = str(js.prompt('You arrived at the destination safe and sound. Now there is a house that has a choice of 3 doors to enter. You can choose the "yellow" door, the "blue" door, or the "red" door. Type one of the colors.')).lower()
-        
-        print(f"You chose: {choice3}\n")
         if choice3 == "yellow":
-            print('You found the treasure! You Win!')
+            print('\nUnbelievable! You found the treasure! You Win!')
         elif choice3 == "blue":
-            print("You were eaten by beasts! Game Over.")
+            print("\nYou were eaten by beasts! Game Over.")
         else:
-            print("You were burned by fire! Game Over.")
+            print("\nYou were burned by fire! Game Over.")
     else:
-        print(f"You chose: {choice2}\n")
-        print("You ran into a school of piranha fish and didn't survive. Game over.")
+        print("\nYou ran into a school of piranha fish and didn't survive. Game over.")
 else:
-    print(f"You chose: {choice1}\n")
-    print("You triggered the booby trap and unlocked the trap door and you fell in. Game over.")
+    print("\nYou triggered the booby trap and unlocked the trap door and you fell in. Game over.")
